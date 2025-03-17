b6888bd5e0b021eda004b5e891df7ea4ff857911
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-03-17 17:13:05 +0500

    quizzes done !

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -7,19 +7,58 @@ from config import settings
 class Order(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     tariff = models.ForeignKey('general.Tariff', on_delete=models.PROTECT)
-    days  = models.IntegerField(default=0)
-    expire_date = models.DateTimeField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    days = models.IntegerField(
+        default=0,
+        blank=True,
+        null=True
+    )
+    expire_date = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+    price = models.FloatField(
+        blank=True,
+        null=True
+    )
+    discount_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
     is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
 
+
 class Transaction(BaseModel):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    state = models.CharField(max_length=100)
-    transaction_id = models.IntegerField()
+    class State(models.IntegerChoices):
+        AWAITING_PAYMENT = 1
+        COMPLETED = 2
+        DOES_NOT_EXIST = -1
+        CANCELLED = -2
+
+    state = models.IntegerField(choices=State, default=State.AWAITING_PAYMENT)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    performed_at = models.DateTimeField(blank=True, null=True)
+    canceled_at = models.DateTimeField(blank=True, null=True)
+    reason = models.IntegerField(blank=True, null=True)
+
+    @staticmethod
+    def to_timestamp(_datetime):
+        return int(_datetime.timestamp() * 1000) if _datetime else 0
+
+    def get_perform_time(self):
+        return self.to_timestamp(self.performed_at)
+
+    def get_canceled_time(self):
+        return self.to_timestamp(self.canceled_at)
+
+    def get_created_time(self):
+        return self.to_timestamp(self.created_at)
 
     def __str__(self):
-        return self.state
+        return self.pk
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,63 +1,7 @@
-from django.db import models
-from django.contrib.auth.hashers import make_password
-from django.core.validators import FileExtensionValidator
-from django.contrib.auth.models import UserManager, AbstractUser
+
+from django.contrib.auth.models import  AbstractUser
 
 from apps.utils.models.base_model import BaseModel
 
-
-
-class CustomUserManager(UserManager):
-
-    def _create_user(self, email, password, **extra_fields):
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.password = make_password(password)
-        user.save(using=self._db)
-        return user
-
-
-    def create_user(self, email=None, password=None, full_name=None, **extra_fields):
-        extra_fields["full_name"] = full_name
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email=None, password=None, full_name=None, **extra_fields):
-        extra_fields['is_staff'] = extra_fields['is_superuser'] = True
-        extra_fields["full_name"] = full_name
-        return self._create_user(email, password, **extra_fields)
-
-
 class CustomUser(BaseModel, AbstractUser):
-    class AuthProviders(models.TextChoices):
-        EMAIL = 'email', "Email"
-        GOOGLE = 'google', "Google"
-        TELEGRAM = 'telegram', "telegram"
-        INSTAGRAM = 'instagram', "instagram"
-
-    username = None
-    email = models.EmailField(unique=True)
-    icon = models.ImageField(upload_to='users/icons/',
-                             blank=True,
-                             null=True,
-                             validators=[FileExtensionValidator(
-                                 allowed_extensions=['jpg', 'jpeg', 'png'])],
-                             )
-
-    objects = CustomUserManager()
-    full_name = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=30, blank=True, null=True)
-    last_name = models.CharField(max_length=30, blank=True, null=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['full_name']
-
-    user_code = models.CharField(max_length=50, unique=True, blank=True, null=True)
-    balance = models.PositiveSmallIntegerField(default=0)
-    coins = models.PositiveSmallIntegerField(default=0)
-    correct_answers = models.FloatField(default=0)
-    last_10_quiz_results_avg = models.FloatField(default=0)
-    order_expire_date = models.DateTimeField(auto_now_add=True)
-    auth_provider = models.CharField(
-        max_length=50,
-        default=AuthProviders.EMAIL)
-
+    pass
