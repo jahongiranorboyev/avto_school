0aad0e562c5737aaed8c3cba7f93f0f95111c192
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-03-20 19:29:42 +0500

    promblems fixed !

--- apps/quizzes/urls.py
+++ apps/quizzes/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
+from .views import (
+    QuestionCategoryListCreateAPIView, QuestionCategoryDetailAPIView,
+    UserQuestionCategoryListAPIView, UserQuestionCategoryDetailAPIView,
+    QuestionListCreateAPIView, QuestionDetailAPIView,
+    SavedQuestionListCreateAPIView, SavedQuestionDetailAPIView,
+    QuizResultCreateView
+)
 
-from apps.quizzes.views import QuestionCategoryListAPIView
+urlpatterns = [
+    path('categories/', QuestionCategoryListCreateAPIView.as_view(), name='question-category-list'),
+    path('categories/<int:pk>/', QuestionCategoryDetailAPIView.as_view(), name='question-category-detail'),
 
-urlpatterns =[
-    path('question-category-list/', QuestionCategoryListAPIView.as_view(), name='question-category-list'),
+    path('user-categories/', UserQuestionCategoryListAPIView.as_view(), name='user-question-category-list'),
+    path('user-categories/<int:pk>/', UserQuestionCategoryDetailAPIView.as_view(), name='user-question-category-detail'),
+
+    path('questions/', QuestionListCreateAPIView.as_view(), name='question-list'),
+    path('questions/<int:pk>/', QuestionDetailAPIView.as_view(), name='question-detail'),
+    path('saved-questions/', SavedQuestionListCreateAPIView.as_view(), name='saved-question-list'),
+    path('saved-questions/<int:pk>/', SavedQuestionDetailAPIView.as_view(), name='saved-question-detail'),
+    path('quiz-results/', QuizResultCreateView.as_view(), name='quiz-result-list'),
 ]
