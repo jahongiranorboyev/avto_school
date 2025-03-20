f8baf995e34b08d0d98fdacd9a6de4cfe326bdce
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-03-20 19:29:42 +0500

    promblems fixed !

--- apps/quizzes/views.py
+++ apps/quizzes/views.py
@@ -1,9 +1,59 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .models import (
+    QuestionCategory, UserQuestionCategory, Question, QuestionVariant,
+    SavedQuestion, CorrectQuestion, IncorrectQuestion,QuizResult
+)
+from .serializers import (
+    QuestionCategorySerializer, UserQuestionCategorySerializer, QuestionSerializer,
+     SavedQuestionSerializer, QuizResultSerializer
+)
 
-from apps.quizzes.models import QuestionCategory
-from apps.quizzes.serializers.question import QuestionCategoryListSerializer
 
-
-class QuestionCategoryListAPIView(generics.ListAPIView):
+# QuestionCategory API
+class QuestionCategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = QuestionCategory.objects.all()
-    serializer_class = QuestionCategoryListSerializer
+    serializer_class = QuestionCategorySerializer
+
+
+class QuestionCategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = QuestionCategory.objects.all()
+    serializer_class = QuestionCategorySerializer
+
+
+# UserQuestionCategory API
+class UserQuestionCategoryListAPIView(generics.ListAPIView):
+    queryset = UserQuestionCategory.objects.all()
+    serializer_class = UserQuestionCategorySerializer
+
+
+class UserQuestionCategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserQuestionCategory.objects.all()
+    serializer_class = UserQuestionCategorySerializer
+
+
+# Question API
+class QuestionListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+# SavedQuestion API
+class SavedQuestionListCreateAPIView(generics.ListCreateAPIView):
+    queryset = SavedQuestion.objects.all()
+    serializer_class = SavedQuestionSerializer
+
+class SavedQuestionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SavedQuestion.objects.all()
+    serializer_class = SavedQuestionSerializer
+
+
+class QuizResultCreateView(generics.CreateAPIView):
+    queryset = QuizResult.objects.all()
+    serializer_class = QuizResultSerializer
+
+
+
