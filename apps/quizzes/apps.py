698beb899fb80f9c0cd544949722e2d1f8c2aede
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-03-20 19:29:42 +0500

    promblems fixed !

--- apps/quizzes/apps.py
+++ apps/quizzes/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class QuizzesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.quizzes'
+
+    def ready(self):
+        import apps.quizzes.signals
