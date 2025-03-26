480d2f123074fbcc9e8b630ffbc6a319efc7b49b
SanjarHikmatov <hikmatovsanjar885@gmail.com>
2025-03-26 21:03:01 +0500

    dfdg

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,3 +85,6 @@ INTERNAL_IPS = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+
+CSP_DEFAULT_SRC = ("'self'", "https://accounts.google.com", "https://ssl.gstatic.com")
+CSP_SCRIPT_SRC = ("'self'", "https://accounts.google.com", "https://ssl.gstatic.com", "'unsafe-inline'", "'unsafe-eval'")
