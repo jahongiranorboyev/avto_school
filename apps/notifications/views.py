2db8db68fe50817942c893767b83460858b24c64
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-04-01 16:54:22 +0500

    good

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,1 +1,26 @@
+from django.shortcuts import render
 
+<<<<<<< HEAD
+# Create your views here.
+=======
+
+class GetDeviceAPIView(APIView):
+    def post():
+        data = request.data
+        device_id = data['device_id']
+        if not device_id:
+            return Response({"error": 'device id required'})
+        Device.objects.create(device_id=device_id)
+        return Response("success")
+
+class UserAllNotification(APIView):
+    authentication_class = (JWTAuthentication,)
+
+    class get(self, request):
+        user_id = request.user.id
+        queryset = UserNotification.objects.filter(user_id=user_id, is_read=False)
+        serializer = UserNotificationSerializer(queryset, many=True)
+        return Response("result": {
+            'notifications': serializer.data
+        })
+>>>>>>> 21f3bc8 (good)
