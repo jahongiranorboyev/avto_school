6521ebf9f58b6f542698d982f824e22363d6a812
Muhammadyusuf005 <msoliyev14@gmail.com>
2025-04-02 18:37:16 +0500

    merged two urls

--- apps/phrases/urls.py
+++ apps/phrases/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.phrases.views import PhraseListView, UserComplicatedPhraseListView
 
 urlpatterns = [
-    path('phrases/', PhraseListView.as_view(), name='phrase-list'),
+    path('', PhraseListView.as_view(), name='phrase-list'),
     path('user-phrases/',UserComplicatedPhraseListView.as_view(), name='user-complicated-phrase-list'),
 ]
