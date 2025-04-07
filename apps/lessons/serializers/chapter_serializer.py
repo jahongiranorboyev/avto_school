a29a9b69f758303304a3dac7bd36a8b1de96591e
Muhammadyusuf005 <msoliyev14@gmail.com>
2025-04-07 18:28:43 +0500

    eventually errors are fixed

--- apps/lessons/serializers/chapter_serializer.py
+++ apps/lessons/serializers/chapter_serializer.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from rest_framework import serializers
 
 from apps.lessons.models import Chapter
@@ -7,3 +8,10 @@ class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapter
         fields = '__all__'
+
+    def validate_duration(self, value):
+        lessons = value.lessons_set.all()
+        if not lessons.exists():
+            return 0
+        total_duration = lessons.aggregate(total=Sum('duration'))
+        return total_duration or 0
--- apps/phrases/admin.py
+++ apps/phrases/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from apps.phrases.models import Phrase, UserCompletedPhrase
 
-# Register your models here.
+
+@admin.register(Phrase)
+class PhraseAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
+    list_filter = ("title",)
+    search_fields = ("title", "description")
+    ordering = ("created_at", )
+
+
+@admin.register(UserCompletedPhrase)
+class UserCompletedPhraseAdmin(admin.ModelAdmin):
+    list_display = ("user__full_name", "phrase__title" )
+    list_filter = ("user__full_name", "phrase__title")
+    search_fields = ("user__name", "phrase__description")
+    ordering = ("created_at", )
