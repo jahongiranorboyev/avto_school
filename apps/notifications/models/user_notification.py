1e5deb00bd0311bcbe2639fd5ae92af677a9d207
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-04-07 07:03:11 +0500

    natofications and books apps changed

---
+++ apps/notifications/models/user_notification.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+
+from apps.utils.models import BaseModel
+
+
+class UserNotification(BaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    system_notification = models.ForeignKey('notifications.SystemNotification', on_delete=models.PROTECT,related_name='sys_notifications')
+    custom_notification = models.ForeignKey('notifications.CustomNotification', on_delete=models.PROTECT,related_name='custom_notifications')
+    is_read = models.BoolenField(default=False)
+    
+    def __str__(self):
+        return self.user.id
