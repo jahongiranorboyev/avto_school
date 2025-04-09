01c9b321313ff0d7796f412b56dbf534a05376ea
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-04-09 09:35:47 +0500

    dfsf

--- apps/notifications/views/notification.py
+++ apps/notifications/views/notification.py
@@ -1,11 +1,1 @@
-class UserAllNotification(APIView):
-    authentication_class = (JWTAuthentication,)
-
-    class get(self, request):
-        user_id = request.user.id
-        queryset = UserNotification.objects.filter(user_id=user_id, is_read=False)
-        serializer = UserNotificationSerializer(queryset, many=True)
-        return Response("result": {
-            'notifications': serializer.data
-        })
 
