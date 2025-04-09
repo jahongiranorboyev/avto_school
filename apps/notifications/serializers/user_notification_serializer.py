aca90c2cb545e1a3b0301ba834e7f966909209cb
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-04-09 09:35:47 +0500

    dfsf

--- apps/notifications/serializers/user_notification_serializer.py
+++ apps/notifications/serializers/user_notification_serializer.py
@@ -1,8 +1,11 @@
-from django.rest_fremwork import ModelSerializer
+from rest_framework import serializers
+
+from apps.notifications.models import UserNotification
+from apps.notifications.serializers.custom_notification_serializer import CustomNotificationserializer
+from apps.notifications.serializers.system_notification_serializer import system_notification
 
 
-
-class UserNotificationSerializer(serializer.ModelSerializer):
+class UserNotificationSerializer(serializers.ModelSerializer):
     custom_notification = CustomNotificationserializer(read_only=True)
     system_notification = system_notification(read_only=True)
     class Meta:
