90c0b35a950fc3e4ed0cfd69f575657025477f87
bekjon <bekjonabdurasulov21@gmail.com>
2025-04-14 18:34:09 +0500

    Yangi o‘zgarishlar

--- apps/general/urls.py
+++ apps/general/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from apps.general.views import tariff,level,report
+from apps.general.views import level,report
 
 
 urlpatterns =[
-    path('tariff/', tariff.TariffListCreateAPIView.as_view(), name='tariff-create-list'),
     path('level/', level.LevelCreateListAPIView.as_view(), name='level-create-list'),
-    path('report/',tariff.TariffListCreateAPIView.as_view(), name='report-create-list'),
+    path('report/',report.ReportListCreateAPIView.as_view(), name='report-create-list'),
 
 ]
