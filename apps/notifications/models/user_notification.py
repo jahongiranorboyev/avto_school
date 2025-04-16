b712b4bd4c24fb6bba71261ba460958fb854d6f1
jahongiranorboyev <jahongiranorboyev8@gmail.com>
2025-04-16 19:16:28 +0500

    ddddqw

--- apps/notifications/models/user_notification.py
+++ apps/notifications/models/user_notification.py
@@ -8,11 +8,6 @@ class UserNotification(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     system_notification = models.ForeignKey('notifications.SystemNotification', on_delete=models.PROTECT,related_name='sys_notifications')
     custom_notification = models.ForeignKey('notifications.CustomNotification', on_delete=models.PROTECT,related_name='custom_notifications')
-<<<<<<< HEAD
-    is_read = models.BooleanField(default=False)
-=======
-    is_read = models.BoolenField(default=False)
->>>>>>> 2061695 (natofications and books apps changed)
-    
+
     def __str__(self):
         return self.user.id
