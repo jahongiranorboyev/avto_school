f2065a1dd01fa5f5dd1cfadf21dceb03729a1861
Muhammadyusuf005 <msoliyev14@gmail.com>
2025-04-16 17:09:25 +0500

    translations are started!

--- apps/lessons/models/lesson.py
+++ apps/lessons/models/lesson.py
@@ -1,26 +1,31 @@
 from django.db import models
 from django.conf import settings
-from django.core.validators import FileExtensionValidator,MinValueValidator,MaxValueValidator
+from django.core.validators import FileExtensionValidator, MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 from apps.utils.models import BaseModel
 
 class Lesson(BaseModel):
-    """ This model represents a lesson for this project"""
+    """ This model represents a lesson for this project """
 
-    title = models.CharField(max_length=100)
-    video = models.FileField(upload_to='lessons/video/%Y/%m/%d', null=True, blank=True,
-                             validators=[FileExtensionValidator(['mp4', 'pptx'])])
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
+    video = models.FileField(
+        upload_to='lessons/video/%Y/%m/%d',
+        null=True,
+        blank=True,
+        validators=[FileExtensionValidator(['mp4'])]
+    )
+    description = models.TextField(null=True, blank=True, verbose_name=_("Description"))
     is_premium = models.BooleanField(default=False)
     duration = models.PositiveSmallIntegerField(default=0)
-    chapter = models.ForeignKey('lessons.Chapter', on_delete=models.PROTECT, related_name='lessons')
+    chapter = models.ForeignKey('lessons.Chapter', on_delete=models.PROTECT, related_name='chapter_lessons')
 
     def __str__(self):
         return self.title
 
 
 class LessonResource(BaseModel):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
     url = models.URLField(null=True, blank=True)
     lesson = models.ForeignKey('lessons.Lesson', on_delete=models.PROTECT)
 
@@ -29,8 +34,8 @@ class LessonResource(BaseModel):
 
 
 class LessonTerm(BaseModel):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=100,verbose_name=_("Name"))
+    description = models.TextField(null=True, blank=True, verbose_name=_("Description"))
     lesson = models.ForeignKey('lessons.Lesson', on_delete=models.PROTECT)
 
     def __str__(self):
@@ -41,7 +46,7 @@ class UserLesson(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     lesson = models.ForeignKey('lessons.Lesson', on_delete=models.PROTECT)
     completed_at = models.DateTimeField(null=True, blank=True)
-    rating = models.PositiveSmallIntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(5)])
+    rating = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
 
     def __str__(self):
         return str(self.user.id)
