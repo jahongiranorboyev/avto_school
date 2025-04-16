2fa0d8797e8b1e090731a37632a07053df56ad45
bekjon <bekjonabdurasulov21@gmail.com>
2025-04-16 19:01:56 +0500

    aaaa

--- apps/notifications/models/user_notification.py
+++ apps/notifications/models/user_notification.py
@@ -9,5 +9,8 @@ class UserNotification(BaseModel):
     system_notification = models.ForeignKey('notifications.SystemNotification', on_delete=models.PROTECT,related_name='sys_notifications')
     custom_notification = models.ForeignKey('notifications.CustomNotification', on_delete=models.PROTECT,related_name='custom_notifications')
 
+    is_read = models.BooleanField(default=False)
+
+    
     def __str__(self):
         return self.user.id
