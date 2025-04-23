adbede1e220c6e40fc97bad93fe54fb1f5c53709
bekjon <bekjonabdurasulov21@gmail.com>
2025-04-23 10:48:37 +0500

    docker create

--- apps/general/views/tariff.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-
-from apps.general.models.tariff import Tariff
-from apps.general.serializers.tariff import TariffSerializer
-
-
-class TariffListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Tariff.objects.all()
-    serializer_class = TariffSerializer
