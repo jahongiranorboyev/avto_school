c1c2b99448cbed7a2f74ef845ef1e51ab7bfdfc5
bekjon <bekjonabdurasulov21@gmail.com>
2025-04-23 10:49:30 +0500

    docker create2

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from apps.general.models import Tariff
+# from apps.general.models import Tariff
 from apps.payments.models import Order, Transaction
 from apps.utils.services.payments.generate_url import generate_payment_url
 from apps.utils.services.payments.check_payment_functions import validation
