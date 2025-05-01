be672f857a81113f2362f23451b9ff96250057c7
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-05-01 19:30:12 +0500

    aaaa

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book, BookChapter, BookPage, UserBookChapter
 
-# Register your models here.
+class BookInline(admin.TabularInline):
+    model = Book
+
+class BookChapterInline(admin.TabularInline):
+    model = BookChapter
+
+
+class BookPageInline(admin.TabularInline):
+    model = BookPage
+
+class UserBookChapterInline(admin.TabularInline):
+    model = UserBookChapter
+
+
+admin.site.register(Book)
+admin.site.register(BookChapter)
+admin.site.register(BookPage)
+admin.site.register(UserBookChapter)
--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -5,3 +5,4 @@ from .middleware import *
 from .installed_apps import *
 from .rest_framework import *
 from .auth_password_validators import *
+from .ckeditor import *
