486808071aecc51689533d8ff7f4776ae55fb289
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-05-07 20:19:05 +0500

    notification finished

--- apps/notifications/serializers/user_notification_serializer.py
+++ apps/notifications/serializers/user_notification_serializer.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 
 from apps.notifications.models import UserNotification
 from apps.notifications.serializers.custom_notification_serializer import CustomNotificationserializer
-from apps.notifications.serializers.system_notification_serializer import system_notification
+from apps.notifications.serializers.system_notification_serializer import SystemNotificationSerializer
 
 
 class UserNotificationSerializer(serializers.ModelSerializer):
     custom_notification = CustomNotificationserializer(read_only=True)
-    system_notification = system_notification(read_only=True)
+    system_notification = SystemNotificationSerializer(read_only=True)
     class Meta:
         models = UserNotification
         fields = ['custom_notification', 'system_notification']
--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.notifications.views import SystemNotificationViewSet
 from apps.notifications.views.send_notification import SendNotificationToDeviceView
 from apps.notifications.views.get_device_id import RegisterDeviceView
 
+router = DefaultRouter()
+router.register(r'system-notifications', SystemNotificationViewSet, basename='system-notification')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('send-to-device/', SendNotificationToDeviceView.as_view(), name='send_to_device'),
     path('register-device/', RegisterDeviceView.as_view(), name='register_device'),
 ]
